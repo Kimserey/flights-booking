ffd9f59eeaf524693ead8c0bced03038a406e4f3
Kimserey <kimserey.lam@gmail.com>
2018-12-31 19:33:59 +0000

    Added mapbox map on after view init to access div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'flights-booking';
+  map:  mapboxgl.Map;
+
+  constructor() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia2ltc2VyZXkwMSIsImEiOiJjanFjbjdsMjQzeDZlNDNwcDd3eGl1Nzl6In0.hgMiXO9FMV26b8X8TANLAw';  
+  }
+  
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+  }
 }
